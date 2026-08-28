7a13f3191e98208fb2b45c8dec04ed0e0c23eddc
mohmedhaggag1 <haggag794@gmail.com>
2026-08-28 20:59:48 +0300

    login

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -13,22 +13,12 @@ export class AuthService {
   dialog = inject(MatDialog);
   alert = inject(AlertService);
   router = inject(Router)
-
   constructor(private http: HttpClient) { }
 
-  register(data: any) {
-    return this.http.post(`${environment.apiUrl}Auth/RegisterAsyncAdmin`, data);
+  login(data: any) {
+    const endpoint = data.isAdmin ? 'auth/admin/login' : 'auth/academy/login';
+    return this.http.post(`${environment.apiUrl}${endpoint}`, data);
   }
-
-  registerAsAdmin(data: any) {
-    return this.http.post(`${environment.apiUrl}Auth/RegisterAdminByCompanyIdAsync`, data);
-  }
-
- login(data: any) {
-  const endpoint = data.isAdmin? 'auth/admin/login' : 'auth/academy/login';
-  return this.http.post(`${environment.apiUrl}${endpoint}`, data);
-  }
-
 
   logout() {
     let dialogRef = this.dialog.open(ConfirmationMessageComponent, {
@@ -51,36 +41,6 @@ export class AuthService {
   }
 
   changePassword(data: any) {
-    return this.http.post(`${environment.apiUrl}auth/change-password`, data);
-  }
-
-  sendEmail(data: any) {
-    let url = new URL(`${environment.apiUrl}Auth/ForgetPassword`);
-    url.searchParams.append('EmailAddress', data);
-    return this.http.post(`${url}`, {});
-  }
-
-  resetPassword(data: any) {
-    return this.http.post(`${environment.apiUrl}Auth/ResetPassword`, data);
-  }
-
-  checkOtp(otpCode: number, userId: string) {
-    let url = new URL(`${environment.apiUrl}Auth/CheckOTP`);
-    url.searchParams.append('otp', otpCode.toString());
-    url.searchParams.append('userId', userId);
-    return this.http.post(`${url}`, {});
-  }
-
-  resendOtp(userId: any) {
-    return this.http.post(
-      `${environment.apiUrl}Auth/ResendOTP?userId=${userId}`,
-      null
-    );
-  }
-
-  // create Guest account
-  CreateGuestQR(floorId: any) {
-    let url = new URL(`${environment.apiUrl}Auth/RegisterUserGuestAsync?floorID=${floorId}`);
-    return this.http.post(`${url}`, null);
+    return this.http.patch(`${environment.apiUrl}auth/change-password`, data);
   }
 }
