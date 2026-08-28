a7912b517aca4e28796e0db72a321f6398246ab6
mohmedhaggag1 <haggag794@gmail.com>
2026-08-28 12:11:57 +0300

    login

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -10,7 +10,7 @@ import {
 import { Router, RouterModule } from '@angular/router';
 import { EscobaInputComponent } from '../../core/inputs/escoba-input.component';
 import { InputPasswordComponent } from '../../core/inputs/input-password.component';
-// import { AuthService } from '../../core/services/auth.service';
+import { AuthService } from '../../core/services/auth.service';
 import { SubmitButtonComponent } from '../../core/components/submit-button.component';
 import { AlertService } from '../../core/services/alert.service';
 import { MatDialog } from "@angular/material/dialog";
@@ -34,7 +34,7 @@ import { LeftComponent } from '../../core/components/left.component';
   styleUrl: './login.scss'
 })
 export class Login {
-  // service = inject(AuthService);
+  service = inject(AuthService);
   alert = inject(AlertService);
   form!: FormGroup;
   passwordType: string = 'password';
@@ -42,59 +42,31 @@ export class Login {
 
   constructor(private fb: FormBuilder, private router: Router, private dialog: MatDialog) {
     this.form = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required]],
       password: ['', [Validators.required]],
-      rememberCheckbox: [false],
+      isAdmin: [false],
     });
-
-
   }
 
   submit() {
-    // if (this.form.valid) {
-    //   this.loading = true;
-    //   this.service.login(this.form.value).subscribe(
-    //     (res: any) => {
-    //       if (res.success) {
-    //         if (res.data.isExpire) {
-    //           localStorage.setItem('temToken', res.data.token)
-    //           localStorage.setItem('temCompanyId', res.data.companyId);
-    //           localStorage.setItem('role', res.data.roles[0]);
-
-    //           this.dialog.open(RenewDialogComponent, {
-    //             width: '700px'
-    //           });
-    //           return
-    //         }
-    //         if (
-    //           res.data.isAuthenticated == false &&
-    //           res.data.twoFactorEnabled == true
-    //         ) {
-    //           localStorage.setItem('otpAllowed', 'true');
-    //           localStorage.setItem('email', this.form.value.email);
-    //           localStorage.setItem('userIdentityId', res.data.userIdentityId);
-    //           localStorage.setItem('companyCode', res.data.companyCode);
-    //           window.location.href = '/auth/otp';
-    //         } else if (res.data.isAuthenticated == true || res.data.userStatus == 'Approved') {
-    //           localStorage.setItem('token', res.data.token);
-    //           localStorage.setItem('id', res.data.userIdentityId);
-    //           localStorage.setItem('role', res.data.roles[0]);
-    //           if (res.data.roles[0] === 'Admin') {
-    //             localStorage.setItem('companyId', res.data.companyId);
-    //             localStorage.setItem('companyCode', res.data.companyCode);
-    //           }
-    //           window.location.href = '/';
-    //         } else if (res.data.userStatus == 'Pending') {
-    //           this.alert.showAlert('pending_status', 'bg-primary');
-    //           this.loading = false;
-    //         }
-    //       }
-    //     },
-    //     (err) => (this.loading = false)
-    //   );
-    // } else {
-    //   this.form.markAllAsTouched();
-    // }
+    if (this.form.valid) {
+      this.loading = true;
+      this.service.login(this.form.value).subscribe(
+        (res: any) => {
+          if (res.success) {
+            localStorage.setItem('token', res.data.accessToken);
+            localStorage.setItem('userIdentityId', res.data.user.id);
+            localStorage.setItem('username', res.data.user.username);
+            localStorage.setItem('role', res.data.user.role);
+            localStorage.setItem('mustChangePassword', res.data.user.mustChangePassword);
+            window.location.href = '/admin';
+          }
+        },
+        (err) => (this.loading = false)
+      );
+    } else {
+      this.form.markAllAsTouched();
+    }
   }
 
   get f() {
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,88 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertService } from './alert.service';
+import { ConfirmationMessageComponent } from '../components/confirmation-message.component';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  dialog = inject(MatDialog);
+  alert = inject(AlertService);
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  register(data: any) {
+    return this.http.post(`${environment.apiUrl}Auth/RegisterAsyncAdmin`, data);
+  }
+
+  registerAsAdmin(data: any) {
+    return this.http.post(`${environment.apiUrl}Auth/RegisterAdminByCompanyIdAsync`, data);
+  }
+
+  login(data: any) {
+    return this.http.post(`${environment.apiUrl}auth/admin/login`, data);
+  }
+
+  logout() {
+    let dialogRef = this.dialog.open(ConfirmationMessageComponent, {
+      panelClass: 'small-dialog',
+      data: {
+        btn_name: 'confirm',
+        message: 'logout_message',
+        classes: 'bg-primary white',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((res) => {
+      if (res) {
+        const storedFiles = localStorage.getItem('survey_admin_stored_files');
+        let lang = localStorage.getItem('language') || 'en';
+        localStorage.clear();
+        if (storedFiles) {
+          localStorage.setItem('survey_admin_stored_files', storedFiles);
+        }
+
+        localStorage.setItem('language', lang);
+        this.router.navigate(['auth/login']);
+      }
+    });
+  }
+
+  changePassword(data: any) {
+    return this.http.post(`${environment.apiUrl}Auth/ChangePassword`, data);
+  }
+
+  sendEmail(data: any) {
+    let url = new URL(`${environment.apiUrl}Auth/ForgetPassword`);
+    url.searchParams.append('EmailAddress', data);
+    return this.http.post(`${url}`, {});
+  }
+
+  resetPassword(data: any) {
+    return this.http.post(`${environment.apiUrl}Auth/ResetPassword`, data);
+  }
+
+  checkOtp(otpCode: number, userId: string) {
+    let url = new URL(`${environment.apiUrl}Auth/CheckOTP`);
+    url.searchParams.append('otp', otpCode.toString());
+    url.searchParams.append('userId', userId);
+    return this.http.post(`${url}`, {});
+  }
+
+  resendOtp(userId: any) {
+    return this.http.post(
+      `${environment.apiUrl}Auth/ResendOTP?userId=${userId}`,
+      null
+    );
+  }
+
+  // create Guest account
+  CreateGuestQR(floorId: any) {
+    let url = new URL(`${environment.apiUrl}Auth/RegisterUserGuestAsync?floorID=${floorId}`);
+    return this.http.post(`${url}`, null);
+  }
+}
